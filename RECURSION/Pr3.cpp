7e335d8b72b744aa7d60f286e9844f0db47a21da
ARYAMAAN PANDEY <60667917+Aryamaan23@users.noreply.github.com>
2021-03-01 17:49:47 +0530

    Update Pr3.cpp

--- RECURSION/Pr3.cpp
+++ RECURSION/Pr3.cpp
@@ -1,1 +1,19 @@
+//Binary Representation
 
+#include <iostream>
+using namespace std;
+
+
+int fun(int n)
+{
+	if(n == 1)
+		return 0;
+	else
+		return 1 + fun(n / 2);
+}
+int main() {
+	
+	cout<<fun(16);
+	
+	return 0;
+}
